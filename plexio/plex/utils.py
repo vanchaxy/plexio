99cc9cbc85748959eed4ba9a2420560c5fce7e3f
Maksym Ivanchenko <github@ivanchenko.io>
2024-04-03 10:17:39 -0400

    fix plex error handler (#15)
    Co-authored-by: Maksym Ivanchenko <--set>

--- plexio/plex/utils.py
+++ plexio/plex/utils.py
@@ -10,26 +10,26 @@ from plexio.settings import settings
 async def get_json(client, url, params=None):
     if params is None:
         params = {}
-    async with client.get(
-        url,
-        params=params,
-        timeout=settings.plex_requests_timeout,
-    ) as response:
-        try:
+    try:
+        async with client.get(
+            url,
+            params=params,
+            timeout=settings.plex_requests_timeout,
+        ) as response:
             json = await response.json()
-        except ContentTypeError as e:
-            with configure_scope() as scope:
-                response_bytes = await response.read()
-                scope.add_attachment(bytes=response_bytes, filename='attachment.txt')
-                raise e
-        except ClientConnectorError as e:
-            raise HTTPException(
-                status_code=500,
-                detail='Plex server connection error',
-            ) from e
-        except asyncio.TimeoutError as e:
-            raise HTTPException(
-                status_code=500,
-                detail='Plex server timeout error',
-            ) from e
-        return json
+            return json
+    except ContentTypeError as e:
+        with configure_scope() as scope:
+            response_bytes = await response.read()
+            scope.add_attachment(bytes=response_bytes, filename='attachment.txt')
+            raise e
+    except ClientConnectorError as e:
+        raise HTTPException(
+            status_code=500,
+            detail='Plex server connection error',
+        ) from e
+    except asyncio.TimeoutError as e:
+        raise HTTPException(
+            status_code=500,
+            detail='Plex server timeout error',
+        ) from e
