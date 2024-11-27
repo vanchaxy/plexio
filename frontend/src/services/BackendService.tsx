18620bd43de7ca1d2ba13250689fb47b73cbb850
Maksym Ivanchenko <github@ivanchenko.io>
2024-11-27 23:08:28 +0000

    fix backend url

--- frontend/src/services/BackendService.tsx
+++ frontend/src/services/BackendService.tsx
@@ -1,16 +1,17 @@
 import axios from 'axios';
 
-const BACKEND_API_URL = 'http://localhost/api/v1';
-
 export const isServerAliveRemote = async (serverUrl: string, token: string) => {
   try {
-    const response = await axios.get(`${BACKEND_API_URL}/test-connection`, {
-      timeout: 25000,
-      params: {
-        url: serverUrl,
-        token: token,
+    const response = await axios.get(
+      `${window.location.origin}/api/v1/test-connection`,
+      {
+        timeout: 25000,
+        params: {
+          url: serverUrl,
+          token: token,
+        },
       },
-    });
+    );
     return response.data?.success;
   } catch (error) {
     console.error('Error while ping PMS remote:', error);
