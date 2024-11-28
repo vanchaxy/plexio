62b0e3f9e4dbf9f53da363477323647ee8011a21
Maksym Ivanchenko <github@ivanchenko.io>
2024-11-28 12:53:10 +0000

    tag latest on release

--- plexio/settings.py
+++ plexio/settings.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     plex_requests_timeout: int = 20
     cache_type: CacheType = CacheType.memory
     redis_url: str = 'redis://redis:6399/0'
-    plex_matching_token: str = None
+    plex_matching_token: str | None = None
 
 
 settings = Settings()
